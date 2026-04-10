a8efc24b6cf898eba8f1dd8fc8bc485417e9aa92
Berzki <mhdrizky32@gmail.com>
2026-04-10 14:47:30 +0700

    Initial Commit

---
+++ Berzki/BerzkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerzkiApp.swift
+//  Berzki
+//
+//  Created by Muhammad Rizki on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BerzkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Berzki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Berzki
+//
+//  Created by Muhammad Rizki on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
