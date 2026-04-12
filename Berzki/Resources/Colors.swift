667e75a3ff007fd93b84068154ecac51e2798bba
Berzki <mhdrizky32@gmail.com>
2026-04-12 19:49:29 +0700

    first commit

---
+++ Berzki/Resources/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Berzki
+//
+//  Created by Muhammad Rizki on 12/04/26.
+//
+
+import SwiftUI
+
+// MARK: - Color Extension
+extension Color {
+    // Background: Off-white hangat (#F9F6F0)
+    static let appBackground = Color(red: 0.976, green: 0.965, blue: 0.941)
+        
+    // Accent: Amber tua/Burnt orange (#C4751A)
+    static let accentAmber = Color(red: 0.769, green: 0.459, blue: 0.102)
+    
+    // Text: Coklat Espresso (#1E1208)
+    static let textEspresso = Color(red: 0.118, green: 0.071, blue: 0.031)
+}
---
+++ Berzki/Views/SetelWaktuView.swift
@@ -0,0 +1,24 @@
+//
+//  Waktu.swift
+//  Berzki
+//
+//  Created by Muhammad Rizki on 12/04/26.
+//
+
+import SwiftUI
+
+struct SetelWaktuView: View {
+    var body: some View {
+        ZStack {
+            Color.bgPrimary.ignoresSafeArea()
+            
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    WaktuView()
+}
